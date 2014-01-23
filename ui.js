99e37914a2fcc55e95adaab95550b88fdcda7d00
ArvidHaggqvist <arvid.haggqvist@gmail.com>
2014-01-23 13:06:59 +0100

    UI correct square colors

--- ui.js
+++ ui.js
@@ -6,5 +6,20 @@
 	traverseBoard(function(square, index) {
 		gamediv.append("<div class='square " + index + "'>" + ((square !== undefined) ? square.type : '') + "</div>");
 	});
+	$(".square").each(function() {
+		var boardIndex = $(this).attr('class').split(" ")[1];
+		console.log(boardIndex);
+		if(rank(boardIndex) % 2 === 0 ) {
+			if(boardIndex % 2 !== 0) {
+				$(this).addClass('black');
+			}
+		}
+		else {
+			if(boardIndex % 2 === 0) {
+				$(this).addClass('black');
+			}
+		}
+	});
+
 
 })($);
